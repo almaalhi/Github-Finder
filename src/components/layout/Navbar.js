ced1538792bce06db164f8befb04102b869d6ea3
Alex Alvarado <alex.alvarad@gmail.com>
2020-02-25 14:54:58 -0700

    Refactor UserItem to function based component.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,31 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Navbar = props => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
+    </nav>
+  );
+};
 
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github"
-  };
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github"
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
-
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
