2d19f8aa7abbbac38d78f9c305ee32110883469e
Alex Alvarado <alex.alvarad@gmail.com>
2020-02-25 11:00:54 -0700

    Add Users component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar.js";
+import Users from "./components/users/Users.js";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Navbar title='Github Finder' />
+        <Navbar />
+        <Users />
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-export class Navbar extends Component {
+class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
         <h1>
-          <i className='fab fa-github' /> {this.props.title}
+          <i className={this.props.icon} /> {this.props.title}
         </h1>
       </nav>
     );
