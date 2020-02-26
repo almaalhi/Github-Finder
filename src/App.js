a6c9d9110bb8cbaa3f1b82782be930496ad06968
Alex Alvarado <alex.alvarad@gmail.com>
2020-02-26 11:01:39 -0700

    Add search functionality.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar.js";
 import Users from "./components/users/Users.js";
 import axios from "axios";
+import Search from "./components/users/Search";
 import "./App.css";
 
 class App extends Component {
@@ -10,18 +11,21 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get("https://api.github.com/users ");
+  // Search GitHub users
+  searchUsers = async text => {
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
